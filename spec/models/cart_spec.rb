30a28821eb883f73118b7e555a8a7dbe08aa43dc
john.hsieh <john.hsieh@rockmobile.com>
2017-11-17 12:22:20 +0800

    added cart model test cast

---
+++ spec/models/cart_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Cart, type: :model do
+  let(:cart) { Cart.new }
+
+
+  describe "add or get item from cart" do
+    it "can create Cart" do
+    end
+
+  end
+
+end
