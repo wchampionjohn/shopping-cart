d9cbe5d4d288a761a393c0017cb122a9ec2b3314
wchampionjohn <vhitotoyouv@gmail.com>
2018-01-17 15:53:03 +0800

    新增seed資料

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+unless CartFunction.exists?
+  CartFunction.create [
+    { id: 1, title: '運費規則' ,    name: 'costs',      is_open: 1 },
+    { id: 2, title: '滿額折價' ,    name: 'discount',   is_open: 1 },
+    { id: 3, title: '指定商品特價', name: 'special',    is_open: 1 },
+    { id: 4, title: '買A送B' ,      name: 'gift',       is_open: 1 },
+    { id: 5, title: '商品加購' ,    name: 'additional', is_open: 1 }
+  ]
+end
+
+unless Admin.find_by_email('vhitotoyouv@gmail.com').presence?
+  Admin.create email: 'vhitotoyouv@gmail.com', password: 'yoyorock@0505'
+end
+
+unless Product.exists?
+
+  Spec.delete_all
+  Addition.delete_all
+  AdditionProduct.delete_all
+  CostRule.delete_all
+  Gift.delete_all
+  GiftProduct.delete_all
+  SpecialProduct.delete_all
+
+  CostRule.create reach: 0, cost: 100, is_open: 1, is_limited: 0 # 預設基本運費 100
+  DiscountSetting.create condition: 10000, is_limited: 0, percent_off: 10, discount_type: :percent # 預設滿10000折%10
+
+  seeds_product = YAML.load_file 'db/seeds/products.yml'
+
+  seeds_product.each do |product|
+    new_product = Product.new product.except(*['description', 'image_url' ])
+    new_product.description  = product['description'].gsub(/\n/, '<br/><br/>')
+    new_product.remote_image_path_url = product["image_url"]
+    new_product.status = :all_new
+    new_product.is_launched = 1
+    new_product.save!
+  end
+end
